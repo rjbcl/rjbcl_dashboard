9b49d0b29efa1ff6f660e06cdbd67810aaf40bee
Manoj Gautam <manojit.gautam@gmail.com>
2025-10-05 19:55:10 +0545

    template integrated

---
+++ src/rjbcl_dashboard/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def login(request):
+    return render(request, 'login.html')
