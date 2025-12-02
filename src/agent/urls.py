2ec0058c201e7ac67608e096823db9acf98d5adc
Rayal Khatri <kcrayalgw@gmail.com>
2025-12-02 12:46:57 +0545

    removed unnecessary files

--- src/agent/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('register/', views.register_agent, name='register_agent'),
-    path('login/', views.login_agent, name='login_agent'),
-    path('forget-password/', views.forget_password, name='forget_password'),
-]
